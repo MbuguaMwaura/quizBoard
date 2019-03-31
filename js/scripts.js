6220cbd29b4b9c03b2290f46127443291c101791
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-03-31 19:28:39 +0300

    fixes the jsquery file to ad functionality to the input fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function start(){
-    $(".quiz").show();
+function start() {
+    $("#quiz").show();
     $(".info").hide();
 }
 
@@ -9,11 +9,70 @@ function done() {
     var questionThree = $("input#three").val().toUpperCase();
     var questionFour = $("input#four").val().toUpperCase();
     var questionFive = $("input#five").val().toUpperCase();
-  
-    if ((questionOne === "") || (questionTwo === "")||(questionThree === "")||(questionFour === "")||(questionFive === "")) {
-      alert("please answer all the questions");
+
+    if ((questionOne === "") || (questionTwo === "") || (questionThree === "") || (questionFour === "") || (questionFive === "")) {
+        alert("Please answer all the questions");
     } else {
-      $("input#hidden").show();
+        $("input#hidden").show();
     }
     event.preventDefault();
-  }
+}
+
+$(document).ready(function () {
+    $("#quiz form").submit(function (event) {
+        var questionOne = $("input#one").val().toUpperCase();
+        var questionTwo = $("input#two").val().toUpperCase();
+        var questionThree = $("input#three").val().toUpperCase();
+        var questionFour = $("input#four").val().toUpperCase();
+        var questionFive = $("input#five").val().toUpperCase();
+
+
+
+
+        if (questionOne === "B") {
+            $("input#one").addClass("green")
+        } else {
+            $("input#one").addClass("red")
+        }
+
+        if (questionTwo === "C") {
+            $("input#two").addClass("green")
+        } else {
+            $("input#two").addClass("red")
+        }
+        if (questionThree === "D") {
+            $("input#three").addClass("green")
+        } else {
+            $("input#three").addClass("red")
+        }
+        if (questionFour === "D") {
+            $("input#four").addClass("green")
+        } else {
+            $("input#four").addClass("red")
+        }
+        if (questionFive === "A") {
+            $("input#five").addClass("green")
+        } else {
+            $("input#five").addClass("red")
+        }
+
+
+
+        event.preventDefault();
+
+
+    });
+});
+
+var score = 0;
+
+if (Q1 === "A") {
+    score++;
+}
+if (Q2 === "B") {
+    score++;
+}
+
+var percent = score / 2 * 100;
+
+document.getElementById("marks").innerHTML = percent
