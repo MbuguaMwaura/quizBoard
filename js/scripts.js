ddb76edf0dce362e42400e88060867faf5f303e5
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-03-31 18:59:13 +0300

    adds the functionality of the done button to alert the user to answer all questions before the submit button appears

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@ function start(){
     $(".quiz").show();
     $(".info").hide();
 }
+
+function done() {
+    var questionOne = $("input#one").val().toUpperCase();
+    var questionTwo = $("input#two").val().toUpperCase();
+    var questionThree = $("input#three").val().toUpperCase();
+    var questionFour = $("input#four").val().toUpperCase();
+    var questionFive = $("input#five").val().toUpperCase();
+  
+    if ((questionOne === "") || (questionTwo === "")||(questionThree === "")||(questionFour === "")||(questionFive === "")) {
+      alert("please answer all the questions");
+    } else {
+      $("input#hidden").show();
+    }
+    event.preventDefault();
+  }
